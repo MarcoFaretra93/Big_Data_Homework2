f82c2e27eab6dd53677624160e33ed126f9f1493
gabmarini <gab.marini2@gmail.com>
2017-06-05 20:28:45 +0200

    Extractor made, need test

--- estrattore.py
+++ estrattore.py
@@ -1,9 +1,16 @@
 import lxml.html
 import time
 import csv
+import string
+import perPlayerExtractor as ppExtractor
 from urllib2 import urlopen
 
-letters = ['b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','y','z']
+LETTERS = list(string.ascii_lowercase) #['b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','y','z']
+XPATH_PLAYERS_NAMES = "//th[@data-stat='player']/a/text()|//th[@data-stat='player']/strong/a/text()"
+XPATH_PLAYERS_IDS = "//th[@data-append-csv]/@data-append-csv"
+XPATH_PLAYERS_COLLEGE = "//td[@data-stat='college_name']"
+XPATH_PLAYERS_DEBUT = "//td[@data-stat='year_min']/text()"
+XPATH_PLAYERS_LAST_SEASON = "//td[@data-stat='year_max']/text()"
 
 def getStateFromCollege(collegeName):
 	if(collegeName == "null"):
@@ -18,48 +25,56 @@ def getStateFromCollege(collegeName):
 		except Exception:
 			return "no state"
 
-with open('player.csv', 'wb') as csvfile:
-	writer = csv.writer(csvfile, delimiter = '\t')
-	writer.writerow(['ID', 'NAME', 'COLLEGE', 'STATE', 'FROM', 'TO'])
-	for element in letters:
-		url = 'http://www.basketball-reference.com/players/' + element + '/'
-		page = lxml.html.parse(url).getroot()
+def getAllPlayerBaseInfo(sleep = 1, outFile = 'player.csv'):
+	with open(outFile, 'wb') as csvfile:
+		writer = csv.writer(csvfile, delimiter = '\t')
+		writer.writerow(['ID', 'NAME', 'COLLEGE', 'STATE', 'FROM', 'TO'])
+		for element in letters:
+			url = 'http://www.basketball-reference.com/players/' + element + '/'
+			page = lxml.html.parse(url).getroot()
 
-		""" get all name of players """
-		names = page.xpath("//th[@data-stat='player']/a/text()|//th[@data-stat='player']/strong/a/text()")
+			""" get all name of players """
+			names = page.xpath(XPATH_PLAYERS_NAMES)
 
-		""" get all id of players """
-		identificativi = page.xpath("//th[@data-append-csv]/@data-append-csv")
+			""" get all id of players """
+			identificativi = page.xpath(XPATH_PLAYERS_IDS)
 
-		""" get all college of players """
-		collegeTemp = page.xpath("//td[@data-stat='college_name']")
-		college = []
+			""" get all college of players """
+			collegeTemp = page.xpath(XPATH_PLAYERS_COLLEGE)
+			college = []
 
-		for element in collegeTemp:
-			try:
-				college.append(element.getchildren()[0].text)
-			except IndexError:
-				college.append("null")
+			for element in collegeTemp:
+				try:
+					college.append(element.getchildren()[0].text)
+				except IndexError:
+					college.append("null")
 
-		""" get from and to """
-		fromValues = page.xpath("//td[@data-stat='year_min']/text()")
-		toValues = page.xpath("//td[@data-stat='year_max']/text()")
+			""" get from and to """
+			fromValues = page.xpath(XPATH_PLAYERS_DEBUT)
+			toValues = page.xpath(XPATH_PLAYERS_LAST_SEASON)
 
-		""" write csv with all elements """
-		for i in range(len(names)):
-			getStateFromCollege(college[i])
-			row = []
-			row.append(identificativi[i])
-			row.append(names[i])
-			row.append(college[i])
-			row.append(getStateFromCollege(college[i]))
-			row.append(fromValues[i])
-			row.append(toValues[i])
-			writer.writerow(row)
+			""" write csv with all elements """
+			for i in range(len(names)):
+				row = []
+				row.append(identificativi[i])
+				row.append(names[i])
+				row.append(college[i])
+				row.append(getStateFromCollege(college[i]))
+				row.append(fromValues[i])
+				row.append(toValues[i])
+				writer.writerow(row)
 
-		print "finish: " + url
-		time.sleep(1)
+			print "finish: " + url
+			time.sleep(sleep)
 
-print "finish all"
-
+def getStats(outFile = 'stats.tsv'):
+	with open(outFile, 'wb') as tsvfile:
+		writer = csv.writer(tsvfile, delimiter = '\t')
+		writer.writerow(['PLAYER_ID', 'SEASON', 'GAMES_PLAYED', 'PLAYED_MINUTES', 'FIELD_GOALS', 'FIELD_GOALS_ATTEMPTED', 'FIELD_GOALS_PERCENTAGE', '3_FIELD_GOALS', '3_FIELD_GOALS_ATTEMPTED', '3_FIELD_GOALS_PERCENTAGE', '2_FIELD_GOALS', '2_FIELD_GOALS_ATTEMPTED', '2_FIELD_GOALS_PERCENTAGE', 'EFFECTIVE_FIELD_GOALS_PERCENTAGE', 'FREE_THROWS', 'FREE_THROWS_ATTEMPTED', 'FREE_THROWS_PERCENTAGE', 'OFFENSIVE_REBOUNDS', 'DEFENSIVE_REBOUNDS', 'TOTAL_REBOUNDS', 'ASSISTS', 'STEALS', 'BLOCKS', 'TURNOVERS', 'PERSONAL_FOULS', 'POINTS'])
+		with open('player.csv', 'rb') as players:
+			reader = csv.reader(players, delimiter = '\t', )
+			reader.next() #skip header
+			for line in reader:
+				print "doing " + line[1]
+				ppExtractor.writePlayerStat(writer, line[0], line[4], line[5])
 
---
+++ perPlayerExtractor.py
@@ -0,0 +1,152 @@
+import lxml.html
+import time
+import csv
+from urllib2 import urlopen
+
+XPATH_PLAYED_GAMES = "//tr[@id='per_game.<season>']/td[@data-stat='g']|//tr[@id='per_game.<season>']/td[@data-stat='g']/strong"
+XPATH_PLAYED_MINUTES = "//tr[@id='per_game.<season>']/td[@data-stat='mp_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='mp_per_g']/strong"
+XPATH_FIELD_GOALS = "//tr[@id='per_game.<season>']/td[@data-stat='fg_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='fg_per_g']/strong"
+XPATH_FIELD_GOALS_ATTEMPTED = "//tr[@id='per_game.<season>']/td[@data-stat='fga_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='fga_per_g']/strong"
+XPATH_FIELD_GOALS_PERCENTAGE = "//tr[@id='per_game.<season>']/td[@data-stat='fg_pct']|//tr[@id='per_game.<season>']/td[@data-stat='fg_pct']/strong"
+XPATH_3_FIELD_GOALS = "//tr[@id='per_game.<season>']/td[@data-stat='fg3_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='fg3_per_g']/strong"
+XPATH_3_FIELD_GOALS_ATTEMPTED= "//tr[@id='per_game.<season>']/td[@data-stat='fg3a_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='fg3a_per_g']/strong"
+XPATH_3_FIELD_GOALS_PERCENTAGE = "//tr[@id='per_game.<season>']/td[@data-stat='fg3_pct']|//tr[@id='per_game.<season>']/td[@data-stat='fg3_pct']/strong"
+XPATH_2_FIELD_GOALS = "//tr[@id='per_game.<season>']/td[@data-stat='fg2_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='fg2_per_g']/strong"
+XPATH_2_FIELD_GOALS_ATTEMPTED= "//tr[@id='per_game.<season>']/td[@data-stat='fg2a_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='fg2a_per_g']/strong"
+XPATH_2_FIELD_GOALS_PERCENTAGE = "//tr[@id='per_game.<season>']/td[@data-stat='fg2_pct']|//tr[@id='per_game.<season>']/td[@data-stat='fg2_pct']/strong"
+XPATH_EFFECTIVE_FIELD_GOALS_PERCENTAGE = "//tr[@id='per_game.<season>']/td[@data-stat='efg_pct']|//tr[@id='per_game.<season>']/td[@data-stat='efg_pct']/strong"
+XPATH_FREE_THROWS = "//tr[@id='per_game.<season>']/td[@data-stat='ft_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='ft_per_g']/strong"
+XPATH_FREE_THROWS_ATTEMPTED = "//tr[@id='per_game.<season>']/td[@data-stat='fta_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='fta_per_g']/strong"
+XPATH_FREE_THROWS_PERCENTAGE = "//tr[@id='per_game.<season>']/td[@data-stat='ft_pct']|//tr[@id='per_game.<season>']/td[@data-stat='ft_pct']/strong"
+XPATH_OFFENSIVE_REBOUNDS = "//tr[@id='per_game.<season>']/td[@data-stat='orb_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='orb_per_g']/strong"
+XPATH_DEFENSIVE_REBOUNDS = "//tr[@id='per_game.<season>']/td[@data-stat='drb_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='drb_per_g']/strong"
+XPATH_TOTAL_REBOUNDS = "//tr[@id='per_game.<season>']/td[@data-stat='trb_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='trb_per_g']/strong"
+XPATH_ASSISTS = "//tr[@id='per_game.<season>']/td[@data-stat='ast_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='ast_per_g']/strong"
+XPATH_STEALS = "//tr[@id='per_game.<season>']/td[@data-stat='stl_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='stl_per_g']/strong"
+XPATH_BLOCKS = "//tr[@id='per_game.<season>']/td[@data-stat='blk_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='blk_per_g']/strong"
+XPATH_TURNOVERS = "//tr[@id='per_game.<season>']/td[@data-stat='tov_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='tov_per_g']/strong"
+XPATH_PERSONAL_FOULS = "//tr[@id='per_game.<season>']/td[@data-stat='pf_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='pf_per_g']/strong"
+XPATH_POINTS = "//tr[@id='per_game.<season>']/td[@data-stat='pts_per_g']|//tr[@id='per_game.<season>']/td[@data-stat='pts_per_g']/strong"
+
+def applyXpaths(page, season, playerId, resultList):
+    try:
+        resultList.append(page.xpath(XPATH_PLAYED_GAMES.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_PLAYED_MINUTES.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_FIELD_GOALS.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_FIELD_GOALS_ATTEMPTED.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_FIELD_GOALS_PERCENTAGE.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_3_FIELD_GOALS.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_3_FIELD_GOALS_ATTEMPTED.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_3_FIELD_GOALS_PERCENTAGE.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_2_FIELD_GOALS.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_2_FIELD_GOALS_ATTEMPTED.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_3_FIELD_GOALS_PERCENTAGE.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_EFFECTIVE_FIELD_GOALS_PERCENTAGE.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_FREE_THROWS.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_FREE_THROWS_ATTEMPTED.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_FREE_THROWS_PERCENTAGE.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_OFFENSIVE_REBOUNDS.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_DEFENSIVE_REBOUNDS.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_TOTAL_REBOUNDS.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_ASSISTS.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_STEALS.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_BLOCKS.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_TURNOVERS.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_PERSONAL_FOULS.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None)
+    try:
+        resultList.append(page.xpath(XPATH_POINTS.replace('<season>', str(season)))[0].text)
+    except Exception:
+        resultList.append(None) 
+    return resultList
+
+def extractBaseStats(playerId, seasonStart):
+    
+    url = 'http://www.basketball-reference.com/players/' + playerId[0] + '/' + playerId + '.html'
+    page = lxml.html.parse(url).getroot()
+    row = []
+    row = [playerId, str(seasonStart) + '-' + str(seasonStart+1)]
+    row += applyXpaths(page, seasonStart+1, playerId, row)
+    return row
+
+def writePlayerStat(writer, playerId, debut, lastSeason, allSeason = True):
+    if lastSeason == debut:
+        lastSeason += 1
+    if allSeason:
+        for season in range(int(debut), int(lastSeason)):
+            writer.writerow(extractBaseStats(playerId, season))
+    else:
+        for season in range(int(debut), int(debut+4)):
+            writer.writerow(extractBaseStats(playerId, season))
+
+
+# with open(outFile, 'wb') as tsvfile:
+        #writer = csv.writer(tsvfile, delimiter = '\t')
+        #writer.writerow(['PLAYER_ID','SEASON', 'GAMES_PLAYED', 'PLAYED_MINUTES', 'FIELD_GOALS', 'FIELD_GOALS_ATTEMPTED'. 'FIELD_GOALS_PERCENTAGE', '3_FIELD_GOALS', '3_FIELD_GOALS_ATTEMPTED', '3_FIELD_GOALS_PERCENTAGE', '2_FIELD_GOALS', '2_FIELD_GOALS_ATTEMPTED', '2_FIELD_GOALS_PERCENTAGE', 'EFFECTIVE_FIELD_GOALS_PERCENTAGE', 'FREE_THROWS', 'FREE_THROWS_ATTEMPTED', 'FREE_THROWS_PERCENTAGE', 'OFFENSIVE_REBOUNDS', 'DEFENSIVE_REBOUNDS', 'TOTAL_REBOUNDS', 'ASSISTS', 'STEALS', 'BLOCKS', 'TURNOVERS', 'PERSONAL_FOULS', 'POINTS'])
