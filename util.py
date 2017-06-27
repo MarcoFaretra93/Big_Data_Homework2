380dbaad3c3523871e628750e4dc46cb188587fc
gabmarini <gab.marini2@gmail.com>
2017-06-27 12:17:04 +0200

    util aggregation

--- util.py
+++ util.py
@@ -5,6 +5,8 @@ import numpy as np
 import constants
 import types
 import ast
+import sys
+import csv
 from pyspark import SparkContext
 from pyspark.mllib.stat import Statistics
 
@@ -99,4 +101,35 @@ def populate():
 	getAndInsertAllStatsByType('variance')
 	getAndInsertAllStatsByType('max')
 	getAndInsertAllStatsByType('min')
+
+
+if __name__ == '__main__':
+	if sys.argv[1] == "aggregate":
+		with open(sys.argv[2]) as f:
+			reader = csv.reader(f, delimiter='\t')
+			university = ""
+			score = 0
+			alumni = 0
+			for line in reader:
+				try:
+					if university == "":
+						university = line[0]
+						score = float(line[1])
+						alumni = float(line[2])
+					if university != line[0]:
+						print('\t'.join([university, str(score), str(alumni)]))
+						university = line[0]
+						score = float(line[1])
+						alumni = float(line[2])
+					else:
+						score += float(line[1])
+						alumni = float(line[2])
+				except IndexError:
+					reader.next()
+
+			print('\t'.join([university, str(score), str(alumni)]))
+ 
+
+
+
 
