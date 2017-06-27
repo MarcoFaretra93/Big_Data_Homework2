b7b08d0808495c48b87a94ee027934cec621e448
gabmarini <gab.marini2@gmail.com>
2017-06-27 22:43:44 +0200

    test

--- scoring.py
+++ scoring.py
@@ -152,7 +152,7 @@ def analyze(sc, percentage, tresholds, out = False, bonus = None, normalizer = F
 	#scorer = Scorer(percentage, tresholds, bonus, normalizer)
 	#scores = scorer.startScoring(parallel_players)
 	#f = lambda player: score4Player(player, percentage, tresholds, bonus, normalizer)	
-	scores = parallel_players.map(lambda player: (player['player_id'],1))
+	scores = parallel_players.map(lambda player: test(player))
 	if out:
 		util.pretty_print(util.normalize_scores(100,scores.collect()))
 	else:
